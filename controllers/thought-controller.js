7c888dc1c94498538b8bed4d69140c2323d5a099
Chris <chrismanfredijr@gmail.com>
2022-08-21 00:32:46 -0400

    intial thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,36 @@
 const {User, Thought} = require('../models');
 
 const thoughtController = {
-    
+    addThought({params, body}, res){
+        console.log(body);
+        Thought.create(body)
+            .then(({_id}) => {
+                return Thought.findOneAndUpdate(
+                    {_id: params.userId},
+                    {$push: {thoughts: _id}},
+                    {new: true}
+                )
+            })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No User found'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+    removeThought({params}, res) {
+        Thought.findOneAndDelete({_id: params.thoughtId})
+            .then(deletedThought => {
+                if(!deletedThought) {
+                    return res.status(400).json({message: "no thought with this id"})
+                }
+                return Thought.findOneAndUpdate(
+                    {_id: params},
+                    {$pull: {thoughts: params.thoughtId}},
+                    {new: true}
+                )
+            })
+    }
 }

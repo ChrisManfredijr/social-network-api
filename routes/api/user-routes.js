e6fc6d042773185f9f61fe1e8e99cda305d2962a
Chris <chrismanfredijr@gmail.com>
2022-08-20 21:44:32 -0400

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+
+//Set up all get requests at /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+    
+
+//set up GET one, PUT and DELETE at /api/users/:id
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+
+module.exports = router;

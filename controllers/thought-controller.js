5bd2bfdaba3f5d6d3ea198eb43091971225eb223
Chris <chrismanfredijr@gmail.com>
2022-08-21 15:37:43 -0400

    basic user controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,11 +1,35 @@
+const { response } = require('express');
 const {User, Thought} = require('../models');
 
 const thoughtController = {
+    getThought({params}, res) {
+        Thought.find({})
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err);
+            })
+    },
+
+    getThoughtById({params}, res) {
+        Thought.findOne({_id: params.id})
+            .then(dbThoughtData => {
+                if(!dbThoughtData){
+                    res.status(404).json({message: 'User not found'})
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    },
+    
     addThought({params, body}, res){
         console.log(body);
         Thought.create(body)
             .then(({_id}) => {
-                return Thought.findOneAndUpdate(
+                return User.findOneAndUpdate(
                     {_id: params.userId},
                     {$push: {thoughts: _id}},
                     {new: true}
@@ -20,6 +44,7 @@ const thoughtController = {
             })
             .catch(err => res.json(err));
     },
+    
     removeThought({params}, res) {
         Thought.findOneAndDelete({_id: params.thoughtId})
             .then(deletedThought => {
@@ -32,5 +57,14 @@ const thoughtController = {
                     {new: true}
                 )
             })
-    }
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({message: "No pizza found with this id!"});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err=> res.json(err))
+    },
+
 }
